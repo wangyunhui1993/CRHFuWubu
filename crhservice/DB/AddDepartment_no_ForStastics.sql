188052a3d48b3e2ab1f2a29d61108fb5f94fd38d
zhuanglin450 <zhuanglin450@163.com>
2018-06-10 16:32:21 +0800

    add 部门

---
+++ crhservice/DB/AddDepartment_no_ForStastics.sql
@@ -0,0 +1,4 @@
+alter table cooler_maintain_statistics add department_no varchar(50) DEFAULT NULL after problem;
+alter table filter_cloth_statistics add department_no varchar(50) DEFAULT NULL after problem;
+alter table filter_dust_statistics add department_no varchar(50) DEFAULT NULL after problem;
+alter table filter_element_statistics add department_no varchar(50) DEFAULT NULL after problem;
---
+++ crhservice/DB/UpdateDepartment_no_ForStastics.sql
@@ -0,0 +1,4 @@
+UPDATE cooler_maintain_statistics SET department_no = '001001'
+UPDATE filter_cloth_statistics SET department_no = '001001'
+UPDATE filter_dust_statistics SET department_no = '001001'
+UPDATE filter_element_statistics SET department_no = '001001'
